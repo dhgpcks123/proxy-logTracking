e9a17619081f118ea23d910087d21877146193ba
hyechan <dhgpcks123@naver.com>
2021-11-10 18:29:40 +0900

    make impl없이  csr 수동 빈 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import(AppV2Config.class)
 //componentScan 대상 지정해뒀으니까 @Import 써준것. v1, v2, v3 쓰려고 ㅎㅎ
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
