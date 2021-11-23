04b002387c83d0e144c2915fb08657cc7a879fe9
hyechan <dhgpcks123@naver.com>
2021-11-23 22:57:52 +0900

    proxy, Advisor(advise+pointCut)

--- src/test/java/hello/proxy/proxyFactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyFactory/ProxyFactoryTest.java
@@ -47,4 +47,31 @@ public class ProxyFactoryTest {
         Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
         Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        //proxy를 만들껀데 타겟클래스 기반으로 만들거야.
+        //인터페이스 있지만.. 구체클래스로 CGLIB 써서, 만들거야.
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+
+//    참고
+        // 스프링부트 AOP 적용할 때 기본적으로 proxyTargetClass=true로 써서 사용한다.
+        // 왜? 경우에 따라서 동적 JDK, CGLIB 쓰는 게 장점아닌가?
+        // 강의 뒷 부분에 알려준다.
+        // 어쨋든 결과적으로 스프링부트는 인터페이스가 있어도 항상 CGLIB를
+        // 사용해서 구체 클래스를 기반으로 프록시를 생성한다.
+    }
 }
